3fcef86c9c2dc7e1c36216c45bf842313fda15f2
Kyle Boos <kyle.boos21@gmail.com>
2013-07-31 16:53:19 -0700

    added archive

--- mysite/static/js/kbjs.js
+++ mysite/static/js/kbjs.js
@@ -9,20 +9,25 @@ $("#tab-container li").click(function(){
   var name = $(this).attr('id');
   if(name=='about-tab'){
 	$(".narrow-content div").addClass("hidden");
+	$("#archive-content").addClass("hidden");
 	$("#about-content").removeClass("hidden");
 	
   }
   if(name=='projects-tab'){
 	$(".narrow-content div").addClass("hidden");
+	$("#archive-content").addClass("hidden");
 	$("#project-content").removeClass("hidden");
   }
   if(name=='resume-tab'){
 	$(".narrow-content div").addClass("hidden");
+	$("#archive-content").addClass("hidden");
 	$("#resume-content").removeClass("hidden");
   }
   if(name=='blog-tab'){
 	$(".narrow-content div").addClass("hidden");
 	$("#blog-content").removeClass("hidden");
+	$("#archive-content").removeClass("hidden");
+
   }
 });
 
